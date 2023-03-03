ece999d1bdb8b5237f118ce99c3569da7b5fc12d
yesmore <3224266014@qq.com>
2023-03-03 20:11:15 +0800

    style: icon and doc

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,94 +1,107 @@
-import { createSignal, For, Show } from 'solid-js'
-import MessageItem from './MessageItem'
-import IconClear from './icons/Clear'
-import type { ChatMessage } from '../types'
+import { createSignal, For, Show } from "solid-js";
+import MessageItem from "./MessageItem";
+import IconClear from "./icons/Clear";
+import type { ChatMessage } from "../types";
 
 export default () => {
-  let inputRef: HTMLInputElement
-  const [messageList, setMessageList] = createSignal<ChatMessage[]>([])
-  const [currentAssistantMessage, setCurrentAssistantMessage] = createSignal('')
-  const [loading, setLoading] = createSignal(false)
+  let inputRef: HTMLInputElement;
+  const [messageList, setMessageList] = createSignal<ChatMessage[]>([]);
+  const [currentAssistantMessage, setCurrentAssistantMessage] =
+    createSignal("");
+  const [loading, setLoading] = createSignal(false);
 
   const handleButtonClick = async () => {
-    const inputValue = inputRef.value
+    const inputValue = inputRef.value;
     if (!inputValue) {
-      return
+      return;
     }
-    setLoading(true)
+    setLoading(true);
     // @ts-ignore
-    if (window?.umami) umami.trackEvent('chat_generate')
-    inputRef.value = ''
+    if (window?.umami) umami.trackEvent("chat_generate");
+    inputRef.value = "";
     setMessageList([
       ...messageList(),
       {
-        role: 'user',
+        role: "user",
         content: inputValue,
       },
-    ])
+    ]);
 
-    const response = await fetch('/api/generate', {
-      method: 'POST',
+    const response = await fetch("/api/generate", {
+      method: "POST",
       body: JSON.stringify({
         messages: messageList(),
       }),
-    })
+    });
     if (!response.ok) {
-      throw new Error(response.statusText)
+      throw new Error(response.statusText);
     }
-    const data = response.body
+    const data = response.body;
     if (!data) {
-      throw new Error('No data')
+      throw new Error("No data");
     }
-    const reader = data.getReader()
-    const decoder = new TextDecoder('utf-8')
-    let done = false
+    const reader = data.getReader();
+    const decoder = new TextDecoder("utf-8");
+    let done = false;
 
     while (!done) {
-      const { value, done: readerDone } = await reader.read()
+      const { value, done: readerDone } = await reader.read();
       if (value) {
-        let char = decoder.decode(value)
-        if (char === '\n' && currentAssistantMessage().endsWith('\n')) {
-          continue
+        let char = decoder.decode(value);
+        if (char === "\n" && currentAssistantMessage().endsWith("\n")) {
+          continue;
         }
         if (char) {
-          setCurrentAssistantMessage(currentAssistantMessage() + char)
+          setCurrentAssistantMessage(currentAssistantMessage() + char);
         }
       }
-      done = readerDone
+      done = readerDone;
     }
     setMessageList([
       ...messageList(),
       {
-        role: 'assistant',
+        role: "assistant",
         content: currentAssistantMessage(),
       },
-    ])
-    setCurrentAssistantMessage('')
-    setLoading(false)
-  }
+    ]);
+    setCurrentAssistantMessage("");
+    setLoading(false);
+  };
 
   const clear = () => {
-    inputRef.value = ''
-    setMessageList([])
-    setCurrentAssistantMessage('')
-  }
+    inputRef.value = "";
+    setMessageList([]);
+    setCurrentAssistantMessage("");
+  };
 
   return (
     <div my-6>
-      <For each={messageList()}>{(message) => <MessageItem role={message.role} message={message.content} />}</For>
-      { currentAssistantMessage() && <MessageItem role="assistant" message={currentAssistantMessage} /> }
-      <Show when={!loading()} fallback={() => <div class="h-12 my-4 flex items-center justify-center bg-slate bg-op-15 text-slate rounded-sm">AI is thinking...</div>}>
+      <For each={messageList()}>
+        {(message) => (
+          <MessageItem role={message.role} message={message.content} />
+        )}
+      </For>
+      {currentAssistantMessage() && (
+        <MessageItem role="assistant" message={currentAssistantMessage} />
+      )}
+      <Show
+        when={!loading()}
+        fallback={() => (
+          <div class="h-12 my-4 flex items-center justify-center bg-slate bg-op-15 text-slate rounded-sm">
+            AI is thinking...
+          </div>
+        )}>
         <div class="my-4 flex items-center gap-2">
           <input
             ref={inputRef!}
             type="text"
             id="input"
             placeholder="Enter something..."
-            autocomplete='off'
+            autocomplete="off"
             autofocus
             disabled={loading()}
             onKeyDown={(e) => {
-              e.key === 'Enter' && !e.isComposing && handleButtonClick()
+              e.key === "Enter" && !e.isComposing && handleButtonClick();
             }}
             w-full
             px-4
@@ -103,14 +116,35 @@ export default () => {
             placeholder:text-slate-400
             placeholder:op-30
           />
-          <button onClick={handleButtonClick} disabled={loading()} h-12 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 text-slate rounded-sm>
-            Send
+          <button
+            onClick={handleButtonClick}
+            disabled={loading()}
+            h-12
+            px-4
+            py-2
+            bg-slate
+            bg-op-15
+            hover:bg-op-20
+            text-slate
+            rounded-sm>
+            🍭
           </button>
-          <button title='Clear' onClick={clear} disabled={loading()} h-12 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 text-slate rounded-sm>
+          <button
+            title="Clear"
+            onClick={clear}
+            disabled={loading()}
+            h-12
+            px-4
+            py-2
+            bg-slate
+            bg-op-15
+            hover:bg-op-20
+            text-slate
+            rounded-sm>
             <IconClear />
           </button>
         </div>
       </Show>
     </div>
-  )
-}
+  );
+};
