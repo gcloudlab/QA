c04444a8f4eaf02e49862c44f95e4083281dbe41
yesmore <3224266014@qq.com>
2023-03-04 10:23:31 +0800

    fix localStorage is not defined

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,9 +1,16 @@
 export const setCustomKey = (key: string) => {
-  if (key) {
+  if (typeof window !== "undefined" && key) {
     localStorage.setItem("custom-key", key);
   }
 };
-export const getCustomKey = () => localStorage.getItem("custom-key") || "";
+export const getCustomKey = () => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("custom-key") || "";
+  }
+  return "";
+};
 export const clearCustomKey = () => {
-  localStorage.removeItem("custom-key");
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("custom-key");
+  }
 };
