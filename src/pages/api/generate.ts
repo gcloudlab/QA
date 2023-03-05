9ceea0b89ad18818557420f6a289ae9b094c8820
yesmore <3224266014@qq.com>
2023-03-05 15:59:45 +0800

    add request limit

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -6,6 +6,11 @@ import { fetch, ProxyAgent } from "undici";
 
 const apiKey = import.meta.env.OPENAI_API_KEY || "";
 const https_proxy = import.meta.env.HTTPS_PROXY || "";
+const THRESHOLD = 1;
+const rateLimit = {
+  count: 0,
+  lastAccessTime: Date.now(),
+};
 
 export const post: APIRoute = async (context) => {
   const body = await context.request.json();
@@ -15,6 +20,19 @@ export const post: APIRoute = async (context) => {
   if (!messages) {
     return new Response("No input text");
   }
+  const now = Date.now();
+
+  if (now - rateLimit.lastAccessTime < 4000) {
+    // 1000毫秒=1秒
+    if (rateLimit.count >= THRESHOLD) {
+      return new Response("⚠ Request is too fast, please wait a second~");
+    }
+    rateLimit.count++;
+  } else {
+    rateLimit.count = 1;
+    rateLimit.lastAccessTime = now;
+  }
+
   const initOptions = generatePayload(
     customKey !== "" ? customKey : apiKey,
     messages
