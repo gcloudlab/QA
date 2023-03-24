2a77bbc3cfefad40c81de54d4df4c12ec63fef63
songjunxi <songjunxi@fdsure.com>
2023-03-24 13:35:28 +0800

    upd: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,7 +20,7 @@ export default () => (
           xmlns="http://www.w3.org/2000/svg">
           <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path>
         </svg>
-        <span text-slate-800>二维码打赏助力</span>
+        <span text-slate-800>二维码打赏</span>
       </a>
       <a
         class="flex max-w-fit items-center justify-center space-x-2 rounded-1 border border-gray-300 bg-gray-3 px-3 py-2 text-sm text-gray-600 shadow-sm transition-colors hover:border-gray-500 hover:bg-gray-6 hover:text-slate-2"
