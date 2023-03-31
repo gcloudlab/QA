10d0d8be9113592b8efb6f2e327453618fbe4709
songjunxi <3224266014@qq.com>
2023-03-31 10:22:10 +0800

    feat: devlop with cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import solidJs from "@astrojs/solid-js";
 import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/edge";
 import netlify from "@astrojs/netlify/edge-functions";
+import cloudflare from "@astrojs/cloudflare";
 import vercelDisableBlocks from "./plugins/vercelDisableBlocks";
 
 const envAdapter = () => {
@@ -12,6 +13,8 @@ const envAdapter = () => {
     return vercel();
   } else if (process.env.OUTPUT === "netlify") {
     return netlify();
+  } else if (process.env.OUTPUT === "cloudflare") {
+    return cloudflare();
   } else {
     return node({
       mode: "standalone",
@@ -28,6 +31,7 @@ export default defineConfig({
     plugins: [
       process.env.OUTPUT === "vercel" && vercelDisableBlocks(),
       process.env.OUTPUT === "netlify" && vercelDisableBlocks(),
+      process.env.OUTPUT === "cloudflare" && vercelDisableBlocks(),
     ],
   },
 });
