73481c853295d3e8b7fbbda08997d6720fd040de
Juiçe <3224266014@qq.com>
2023-04-06 11:28:05 +0800

    Update Recomment.tsx

--- src/components/Recomment.tsx
+++ src/components/Recomment.tsx
@@ -3,8 +3,5 @@ import { OFFICIAL_ACCOUNT_LINK } from "@/contants";
 export default () => (
   <div class="z-1 flex flex-col items-center">
     <img class=" rounded-md" src={OFFICIAL_ACCOUNT_LINK} h-24 />
-<!--     <p text-pink-7 text-sm mt-2 font-700>
-      扫码关注公众号<br />获取最新授权码
-    </p> -->
   </div>
 );
